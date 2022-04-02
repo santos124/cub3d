36edd99abd54753fe0cfda427ed25e1e189eebdd
Waltyr Adina <wadina@oa-o1.msk.21-school.ru>
2022-04-03 00:34:32 +0300

    final 4

--- SRC/moves_left_right.c
+++ SRC/moves_left_right.c
@@ -47,7 +47,7 @@ void	rot_left(t_game *game)
 	i = 0;
 	while (i < 5)
 	{
-		game->plr->angle -= ROTATE_ANGLE / 10;
+		game->plr->angle -= ROTATE_ANGLE / 5;
 		if (game->plr->angle < M_PI * 2 * (-10))
 			game->plr->angle = 2 * M_PI + game->plr->angle;
 		i++;
@@ -61,7 +61,7 @@ void	rot_right(t_game *game)
 	i = 0;
 	while (i < 5)
 	{
-		game->plr->angle += ROTATE_ANGLE / 10;
+		game->plr->angle += ROTATE_ANGLE / 5;
 		if (game->plr->angle > M_PI * 2 * 10)
 			game->plr->angle = 2 * M_PI - game->plr->angle;
 		i++;
